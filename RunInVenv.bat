6a3f702946a1326a02f88e5d86b7acfa9310eb39
JoelMahon <mahon.joel@gmail.com>
2020-02-08 15:28:20 +0000

    removed my .anki2 path (which would cause it to error out of the box)

--- RunInVenv.bat
+++ RunInVenv.bat
@@ -1,3 +1,3 @@
 pushd %~dp0
 call .\venv\Scripts\activate
-python .\AnkiAudioMaker.py -d "Core 2000" -c "%APPDATA%\Anki2\Joel\collection.anki2" Vocabulary-Audio Audio 4000 Vocabulary-English en 500
+python .\AnkiAudioMaker.py -d "Core 2000" -c "" Vocabulary-Audio Audio 4000 Vocabulary-English en 500
